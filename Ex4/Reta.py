cd306bf8747cc196099fda16e470c369f3363ce3
guilhermedd <59738693+guilhermedd@users.noreply.github.com>
2022-06-15 11:42:23 -0300

    funfa

--- Ex4/Reta.py
+++ Ex4/Reta.py
@@ -1,24 +1,27 @@
-import math
 import Ponto2D
 
+
 class Reta:
-     def __init__(self, a, b):
-          self.__a = a (int)
-          self.__b = b (int)
-     
-     def getB(self):
-          return self.__b
+    def __init__(self, a: float, b: float):
+        self.__a = a
+        self.__b = b
 
-     def getA(self):
-          return self.__a
+    def get_b(self):
+        return self.__b
 
-     def intercepta(self, reta):
-          if self.__a == reta.getA() and self.__b != reta.getB():
-               # Se forem paralelas, com todos os pontos diferentes (a = a' e b != b') nao se interceptam, caso contrario se interceptam.
-               return False
-          return True
+    def get_a(self):
+        return self.__a
 
-     def estaNaReta(self, ponto: Ponto2D):
-          if ponto.getY() - (self.__a * ponto.getX()) == self.__b:
-               return True
-          return False
+    def intercepta(self, reta):
+        if self.__a == reta.get_a() and self.__b != reta.get_b():
+            # Se forem paralelas, com todos os pontos diferentes (a = a' e b != b') nao se interceptam, caso contrario se interceptam.
+            return False
+        return True
+
+    def esta_na_reta(self, ponto: Ponto2D):
+        if ponto.get_y() == self.__a * ponto.get_x() + self.__b:
+            return True
+        return False
+
+    def __str__(self):
+        return 'A: {} B: {}'.format(self.__a, self.__b)
