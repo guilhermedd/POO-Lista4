e2f3a9cfe5fd15fbc1add9999ee7d56da504214f
guilhermedd <59738693+guilhermedd@users.noreply.github.com>
2022-06-15 11:41:46 -0300

    PRONTO

--- Ex4/main.py
+++ Ex4/main.py
@@ -1,11 +1,24 @@
-import EspacoGeometrico
-import Ponto2D
-import Reta
+from Ponto2D import Ponto2D
+from Reta import Reta
+from EspacoGeometrico import EspacoGeometrico
+from ObjetoSobrepostoException import ObjetoSobrepostoException
 
-D = Ponto2D(3,2)
-r1 = Reta(1,2)
-r2 = Reta(2,-2)
 
+A = Ponto2D(3, 4)
+B = Ponto2D(2, 3)
+C = Ponto2D(4, 6)
+VERMELHO = Reta(1, 2)
+VERDE = Reta(1, 1)
+AZUL = Reta(2, -2)
 espaco = EspacoGeometrico()
-espaco.adicionarReta(r1)
-espaco.adicionarPonto(D)
+
+try:
+    espaco.adicionar_reta(VERMELHO)
+    espaco.adicionar_reta(VERDE)
+    espaco.adicionar_reta(AZUL)
+    espaco.adicionar_ponto(A)
+    espaco.adicionar_ponto(B)
+    espaco.adicionar_ponto(C)
+except ObjetoSobrepostoException as e:
+    print(e)
+
