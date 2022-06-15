82b748e09e3e29bad691d2fec5ca1ae2556db539
guilhermedd <59738693+guilhermedd@users.noreply.github.com>
2022-06-15 11:43:26 -0300

    funfa

--- Ex4/EspacoGeometrico.py
+++ Ex4/EspacoGeometrico.py
@@ -1,26 +1,43 @@
-import ObjetoSobrepostoException
-import Ponto2D
-import Reta
+from ObjetoSobrepostoException import ObjetoSobrepostoException
+from Ponto2D import Ponto2D
+from Reta import Reta
+
 
 class EspacoGeometrico():
-     def __init__(self):
-          self.__retas = []
-          self.__pontos = []
 
-     def adicionarReta(self, reta: Reta):
-          if self.__retas == []:
-               self.__retas.append(reta)
-          else:
-               for r in self.__retas:
+    def __init__(self):
+        self.__retas = []
+        self.__pontos = []
+
+    def adicionar_reta(self, reta: Reta):
+
+        if not self.__retas:
+            self.__retas.append(reta)
+        else:
+            for r in self.__retas:
+                try:
                     if r.intercepta(reta):
-                         raise ObjetoSobrepostoException
-               self.__retas.append(reta)
+                        raise ObjetoSobrepostoException('Retas Sobrepostos!')
+                except ObjetoSobrepostoException:
+                    raise ObjetoSobrepostoException('Retas Sobrepostos!')
 
-     def adicionarPonto(self, ponto: Ponto2D):
-          if self.__retas == []:
-               self.__retas.append(ponto)
-          else:
-               for r in self.__retas:
-                    if r.estaNaReta(ponto):
-                         raise ObjetoSobrepostoException
-               self.__pontos.append(ponto)
+            # So pede para verificar se a reta cruza outra reta
+            self.__retas.append(reta)
+
+    def adicionar_ponto(self, ponto: Ponto2D):
+
+        if not self.__retas:
+            self.__pontos.append(ponto)
+        else:
+            for r in self.__retas:
+                try:
+                    if r.esta_na_reta(ponto):
+                        raise ObjetoSobrepostoException('Objetos Sobrepostos!')
+                except ObjetoSobrepostoException:
+                    raise ObjetoSobrepostoException('Objetos Sobrepostos!')
+
+            # So pede para verificar se o ponto cruza alguma reta
+            self.__pontos.append(ponto)
+
+    def retas(self):
+        return len(self.__retas)
